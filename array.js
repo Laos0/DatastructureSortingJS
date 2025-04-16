541cdd4949a0ed02f8a0b770a986cadf6fe58162
laos0 <isonylao@gmail.com>
2025-04-16 18:54:04 -0500

    reverse array

--- array.js
+++ array.js
@@ -1,1 +1,13 @@
 // Learning basics to arrays in js
+
+
+// this is how to reverse an array
+function reverseArray(arr){
+    arr.reverse();
+
+    // You can manaually do it like this
+    // let result = [];
+    // for(let i = arr.length - 1; i >= 0; i--) result.push(arr.[i]);
+    // return result
+}
+
