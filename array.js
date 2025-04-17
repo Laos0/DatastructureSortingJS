26f193426b993b1ff78dd174a07f9af2c7416dba
laos0 <isonylao@gmail.com>
2025-04-16 20:01:50 -0500

    how to remove dupes

--- array.js
+++ array.js
@@ -1,13 +1,40 @@
 // Learning basics to arrays in js
 
 
-// this is how to reverse an array
+// ---------------- this is how to REVERSE an array --------------------- //
 function reverseArray(arr){
     arr.reverse();
-
-    // You can manaually do it like this
-    // let result = [];
-    // for(let i = arr.length - 1; i >= 0; i--) result.push(arr.[i]);
-    // return result
 }
 
+// manual way of reversing an array
+function reverseArrayM(arr){
+    let result = [];
+    for(let i = arr.length - 1; i >= 0; i--){
+        result.push(arr[i]);
+    }
+    return result
+}
+//// ------------------ end of reversing array -------------------- ////
+
+// ----------------- how to remove DUPLICCATES ------------------ //
+function removeDupes(arr){
+    return [...new Set[arr]];
+}
+
+// manual way of removing dupes
+function removeDupesM(arr){
+    let seen = {};
+    let result = [];
+
+    for (let num of arr) {
+        
+        if (!seen[num]) {
+            result.push(num);
+            seen[num] = true;
+        }
+    }
+
+    return result;
+}
+
+//// ---------------- end of removing duplicates ------------------- ////
